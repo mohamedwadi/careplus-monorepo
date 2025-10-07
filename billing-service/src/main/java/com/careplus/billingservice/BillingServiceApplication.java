2ee6dcf2249d68184eb628623047ce01726acb97
mohamedwadi <mwadib300@gmail.com>
2025-10-07 17:22:05 +0200

    hello worde gaza

--- billing-service/src/main/java/com/careplus/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/com/careplus/billingservice/BillingServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BillingServiceApplication {
-    //ff
+    //ffksafasfsafasgsfa
     public static void main(String[] args) {
         SpringApplication.run(BillingServiceApplication.class, args);
     }
